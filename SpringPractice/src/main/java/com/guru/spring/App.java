aa9b28a1c3f73b1b18d82b8ca3545a7c67ccb06f
Guru <annapantula.se@gmail.com>
2017-02-15 21:29:05 +0100

    default app class and aptest

---
+++ SpringPractice/src/main/java/com/guru/spring/App.java
@@ -0,0 +1,13 @@
+package com.guru.spring;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
